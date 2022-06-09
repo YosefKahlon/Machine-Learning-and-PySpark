85f22dd72eb1c0c87a1c00695c28e14741e7ecb8
yosefkahlon <yosefkahlon53@gmail.com>
2022-06-09 22:02:34 +0300

    done

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import findspark
 from pyspark.sql import SQLContext
 from pyspark.sql.functions import *
 
-
 findspark.init()
 sc = SparkContext("local[*]", "Simple App")
 
@@ -21,14 +20,12 @@ starting_with_f = sqlContext.sql(
 print("------------------- q1.1 -------------------------")
 starting_with_f.show()
 
-
 """ ----------------------------------- q1.2 ------------------------------------------------------"""
 
 print("------------------- q1.2 -------------------------")
 ##The average number of pages written
 # by each author but only books in English
 df.filter(df.language == 'English').groupBy(df.author).agg(avg(df.pages)).show()
-
 
 """----------------------------------------------------------------------------------------------- """
 
@@ -114,7 +111,6 @@ with open('price.csv') as csvfile:
             y.append(float(row[0]))
             i = i + 1
 
-
 data_x = np.array(items)
 data_y = np.array(y)
 b = 0
@@ -124,35 +120,32 @@ for i in range(11):
     wi.append(0)
     deriv_w.append(0)
 
-
 # I found  for this number of iterations and  alfa give the best result
-alpha = 0.0001
-for iteration in range(1001):
+alpha = 0.001
+for iteration in range(10000):
     sum = 0
     i = 0
 
-    #Sum for derivative
+    # Sum for derivative
     while i < len(wi):
         sum += (wi[i] * data_x[:, i])
         i = i + 1
 
-    #Derived  for 𝑏
+    # Derived  for 𝑏
     deriv_b = np.mean(1 * ((sum + b) - data_y))
 
     i = 0
-    #Derived for w
+    # Derived for w
     while i < len(wi):
         deriv_w[i] = np.dot(((sum + b) - data_y), data_x[:, i]) * 1.0 / len(data_y)
         i = i + 1
 
-    #set new w and b
+    # set new w and b
     b -= alpha * deriv_b
     i = 0
     while i < len(wi):
         wi[i] -= (alpha * deriv_w[i])
         i = i + 1
-
-
 
 i = 0
 while i < len(Prediction):
@@ -165,18 +158,19 @@ while i < len(Prediction):
 
 print("------------------- q2.2 -------------------------")
 
-#Lost function
+# Lost function
 i = 0
-a = 1 / 2 * len(data_y)
+a = 1 / (2 * len(data_y))
 sum = 0
 
 while i < len(data_y):
-    sum += (np.dot(np.array(data_x[i]), np.array(wi)) + b - data_y[i])
+    sum = sum + math.pow((np.dot(np.array(data_x[i]), np.array(wi)) + b - data_y[i]), 2)
+
     i = i + 1
 
-lost = a * math.pow(sum, 2)
-print("mean squared-error ", lost)
+lost = a * sum
 
+print("mean squared-error ", lost)
 
 """ ----------------------------------- q2.3 ------------------------------------------------------"""
 
@@ -188,7 +182,7 @@ with open("data.csv", "r") as source:
     with open("q3.csv", "w") as result:
         writer = csv.writer(result)
         for r in reader:
-            #add all necessary feature
+            # add all necessary feature
             writer.writerow((r[1], r[2], r[3], r[4], r[5], r[6], r[7], r[8], r[9], r[10], r[12]))
 
 with open("data.csv", "r") as source:
@@ -209,15 +203,14 @@ with open('q3.csv') as csvfile:
     csvReader.__next__()
     i = 0
 
-
     for row in csvReader:
 
-        #empty row
+        # empty row
         if row.__len__() == 0:
             i = i + 1
             continue
 
-        #15% for Test set
+        # 15% for Test set
         if i == 1 or i == 7 or i == 17 or i == 27 or i == 37:
 
             Prediction.append(list(np.float_(row)))
@@ -226,7 +219,7 @@ with open('q3.csv') as csvfile:
             continue
         # 85% for Train set
         else:
-            #data x
+            # data x
             items.append(list(np.float_(row)))
 
             i = i + 1
@@ -250,7 +243,7 @@ with open('fire.csv') as csvfile:
             continue
         # 85% for Train set
         else:
-           #data y
+            # data y
             y.append(float(row[0]))
 
             i = i + 1
@@ -258,7 +251,8 @@ with open('fire.csv') as csvfile:
 data_x = np.array(items)
 data_y = np.array(y)
 
-#The prediction function
+
+# The prediction function
 def h(x, w, b):
     return 1 / (1 + np.exp(-(np.dot(x, w) + b)))
 
@@ -272,9 +266,7 @@ b = 0  # random.random() * 1
 
 alpha = 0.001
 
-
 for iteration in range(100000):
-
     deriv_b = np.mean(1 * ((h(data_x, w, b) - data_y)))
     i = 0
     deriv_w = np.dot((h(data_x, w, b) - data_y), data_x) * 1 / len(data_y)
@@ -296,7 +288,6 @@ print("+--------------------------+---------------------------------- + \n"
       "| rly negative  - not fire |      0       |        3           | \n"
       "+--------------------------+-----------------------------------+")
 
-
 print("accuracy = 1 + 3       4    \n"
       "         -------- =   --    \n"
       "          1+1+0+3      5    \n")
@@ -314,7 +305,6 @@ print("F-measure  = 2 * 1 * 1/2     2    \n"
       "               1 + 1/2       3    \n")
 
 print("------------------- END -------------------------")
-
 
 """
 [5.0208, 1.0, 3.531, 1.5, 2.0, 7.0, 4.0, 62.0, 1.0, 1.0, 29.5]
